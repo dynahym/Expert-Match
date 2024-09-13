012af36a33ac01ec5da76f21cd6ff654396274e8
Dyna-ALISMAIL <ld_alismail@esi.dz>
2024-09-13 18:50:22 +0100

    Data collection

---
+++ backend/data_collection/config.py
@@ -0,0 +1,5 @@
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+API_URL = "https://dblp.org/search/author/api"
+HEADERS = {
+    "Accept": "application/json"
+}
